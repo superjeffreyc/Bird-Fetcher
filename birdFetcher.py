c7c371072d7eb2723c79baec2b0cbb33b536f1a3
Jeffrey Chan <jchan63@binghamton.edu>
2016-11-12 03:26:39 +0000

    Modularization and fixed issue with spaces in city name

--- birdFetcher.py
+++ birdFetcher.py
@@ -43,13 +43,13 @@ def build_response(session_attributes, speechlet_response):
 def get_welcome_response():
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Bird Fetcher skill," \
+    speech_output = "Welcome to the Bird Fetcher skill, " \
                     "Please tell me your location of interest by saying, " \
-                    "Birds near Binghamton, "
+                    "Birds near Binghamton New York "
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Please tell me your location of interest by saying, " \
-                    "Birds near Binghamton, "
+                    "Birds near Binghamton New York "
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -64,51 +64,88 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+# Takes an eBird JSON response and returns a string of all the common names
+def buildBirdListAsString(eBirdData, dataSize):
+    birdSightings = ""
+    
+    for sighting in eBirdData:
+        birdSightings += "%s, " % sighting['comName']
 
+        # Insert the word 'and' before the last sighting for proper grammar
+        dataSize -= 1
+        if dataSize == 1:
+            birdSightings += "and "
+    
+    return birdSightings
+
+# Builds Alexa's Response
+def buildRecentSightingsResponse(eBirdData, city, state, birdSightings):
+    if len(eBirdData) == 0:
+        return "There are no recent sightings for %s %s" % (city, state)
+    else:
+        lastDate = eBirdData[-1]['obsDt'].split()[0]    # Only grab the date, ignore the time
+        
+        if len(eBirdData) == 1:
+            return "The only recent bird seen since %s in %s %s, is a %s" % (lastDate, city, state, birdSightings)
+        else:
+            return "The %s most recent birds seen since %s in %s %s, are %s" % (len(eBirdData), lastDate, city, state, birdSightings)
+
+# Gets the index in the Google Maps JSON response that contains the state name
+def getStateNameIndex(location):
+    for i in range(len(location['address_components'])):
+        if 'administrative_area_level_1' in location['address_components'][i]['types']:
+            return i
+
+# Handler for intent "GetBirdsWithPlace"
 def get_bird_data(intent, session):
 
     card_title = intent['name']
     session_attributes = {}
     should_end_session = True
     speech_output = "I'm not sure what your city is. Please try again by " \
-                    "saying, birds near Binghamton"
+                    "saying, birds near Binghamton New York"
     
-    if 'Location' in intent['slots']:
+    if 'City' in intent['slots']:
         
-        # Try to call Google Maps API and eBird API based on location in intent
         try:
-            # Get the city from the intent and call the Google Maps API
-            city = intent['slots']['Location']['value']
-            mapsURL = "https://maps.googleapis.com/maps/api/geocode/json?address=%s" % city
-            locationData = json.load(urllib2.urlopen(mapsURL))
+            # Get the city from the intent
+            city = intent['slots']['City']['value'].title()
+
+            # Build Google Maps API call and replace all spaces with %20 for HTTP GET parameter
+            mapsURL = "https://maps.googleapis.com/maps/api/geocode/json?address=%s" % city.replace(" ", "%20")
             
-            # Grab the latitude and longitude and call the eBird API
-            latitude = locationData['results'][0]['geometry']['location']['lat']
-            longitude = locationData['results'][0]['geometry']['location']['lng']
+            # Call the Google Maps API to get the latitude and longitude of the city
+            locationData = json.load(urllib2.urlopen(mapsURL))
+
+            # Grab the latitude and longitude from the Google Maps API to pass to the eBird API
+            # If there is more than one city with the same name in different states, check to see if the user specified a state
+            # Otherwise, default to the first entry
+            if 'State' in intent['slots'] and len(intent['slots']['State']) == 2:
+                for location in locationData['results']:
+                    stateIndex = getStateNameIndex(location)
+                    
+                    if location['address_components'][stateIndex]['long_name'] == intent['slots']['State']['value'].title():
+                        state = location['address_components'][stateIndex]['long_name']
+                        latitude = location['geometry']['location']['lat']
+                        longitude = location['geometry']['location']['lng']
+                        break
+            else:
+                stateIndex = getStateNameIndex(locationData['results'][0])
+                state = locationData['results'][0]['address_components'][stateIndex]['long_name']
+                latitude = locationData['results'][0]['geometry']['location']['lat']
+                longitude = locationData['results'][0]['geometry']['location']['lng']
+
+            # Build eBird API call. Set max results returned to 10.
             eBirdURL = "https://ebird.org/ws1.1/data/obs/geo/recent?lng=%f&lat=%f&maxResults=10&fmt=json" % (longitude, latitude)
             eBirdData = json.load(urllib2.urlopen(eBirdURL))
             
-            birdSightings = ""          # List of 10 recent birds seen
-            lastDate = ""               # Oldest date among the 10 recent bird sightings
-            dataSize = len(eBirdData)   # Grab the number of entries (could possibly be less than 10)
+            # Convert the eBird JSON response to a string of bird names
+            birdSightings = buildBirdListAsString(eBirdData, len(eBirdData))
+
+            # Format Alexa's response based on the number of recent sightings
+            speech_output = buildRecentSightingsResponse(eBirdData, city, state, birdSightings)
             
-            for sighting in eBirdData:
-                birdSightings += "%s, " % sighting['comName']
-                dataSize -= 1
-                lastDate = sighting['obsDt'].split()[0]     # Only grab the date, ignore the time
-                
-                # Insert the word 'and' before the last sighting to make it sound better
-                if dataSize == 1:
-                    birdSightings += " and "
-    
-            if len(eBirdData) == 0:
-                speech_output = "There are no recent sightings in %s." % city
-            elif len(eBirdData) == 1:
-                speech_output = "The only recent bird seen since %s in %s is a %s" % (lastDate, city, birdSightings)
-            else:
-                speech_output = "The %s most recent birds seen since %s in %s are %s" % (str(len(eBirdData)), lastDate, city, birdSightings)
-                
-        # Problem with the city name provided        
+        # Problem with the city and/or state provided        
         except:
             should_end_session = False
             
@@ -179,6 +216,14 @@ def lambda_handler(event, context):
     """
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
+
+    """
+    This statement prevents someone else from configuring a skill that sends 
+    requests to this function.
+    """
+    if (event['session']['application']['applicationId'] !=
+            "amzn1.ask.skill.a8960bfa-8f7b-4050-9630-b33f1ecf0e2c"):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
